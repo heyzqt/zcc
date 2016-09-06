76b789a95aff3f9ca826c6fe79dee4e31138bb7d
erque <1062631582@qq.com>
2016-09-06 20:05:52 +0800

    完成引导界面功能+启动界面的功能+添加MainActivity所需要的toolbar图片

--- app/src/main/java/com/zcc/activity/GuideActivity.java
+++ app/src/main/java/com/zcc/activity/GuideActivity.java
@@ -1,19 +1,86 @@
 package com.zcc.activity;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.app.Activity;
+        import android.view.View;
+        import android.view.Window;
+        import android.view.WindowManager;
+        import android.widget.ImageView;
+        import android.widget.LinearLayout;
+        import android.widget.RelativeLayout;
+        import android.content.Intent;
+        import android.widget.ScrollView;
 
-/**
- * Created by heyzqt on 2016/9/6.
- *
- * 引导界面
- */
-public class GuideActivity extends AppCompatActivity{
+public class GuideActivity extends Activity implements OnViewChangeListener{
+
+    private static final String TAG = "GuideActivity";
+    private ScrollLayout mScrollLayout;
+    private ImageView[] imgs;
+    private int count;
+    private int currentItem;
+    private RelativeLayout rl;
+    private RelativeLayout mainRLayout;
+    private LinearLayout pointLLayout;
+    private String ip;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //设置无标题
+        //GuideActivity.this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//去掉Activity上面的状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_guide);
+        init();
     }
+
+
+    /**
+     * 初始化
+     */
+    private void init() {
+        mScrollLayout = (ScrollLayout) findViewById(R.id.ScrollLayout);
+        pointLLayout = (LinearLayout) findViewById(R.id.llayout);
+        mainRLayout = (RelativeLayout) findViewById(R.id.mainRLayout);
+        rl = (RelativeLayout) findViewById(R.id.guide_rl);
+        rl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mScrollLayout.setVisibility(View.GONE);
+                pointLLayout.setVisibility(View.GONE);
+                mainRLayout.setBackgroundResource(R.mipmap.guide_for_olympic);
+                Intent intent = new Intent(GuideActivity.this, MainActivity.class);
+                GuideActivity.this.startActivity(intent);
+                GuideActivity.this.finish();
+            }
+        });
+        count = mScrollLayout.getChildCount();
+        imgs = new ImageView[count];
+        for (int i = 0; i < count; i++) {
+            imgs[i] = (ImageView) pointLLayout.getChildAt(i);
+            imgs[i].setEnabled(true);
+            imgs[i].setTag(i);
+        }
+        currentItem = 0;
+        imgs[currentItem].setEnabled(false);
+        mScrollLayout.SetOnViewChangeListener(this);
+    }
+
+    @Override
+    public void OnViewChange(int position) {
+        setcurrentPoint(position);
+    }
+
+    private void setcurrentPoint(int position) {
+        if (position < 0 || position > count - 1 || currentItem == position) {
+            return;
+        }
+        imgs[currentItem].setEnabled(true);
+        imgs[position].setEnabled(false);
+        currentItem = position;
+    }
+
+
+
 }
--- app/src/main/java/com/zcc/activity/SplashActivity.java
+++ app/src/main/java/com/zcc/activity/SplashActivity.java
@@ -1,19 +1,94 @@
 package com.zcc.activity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
- * Created by heyzqt on 2016/9/6.
+ * Created by zyq on 2016/9/6.
  *
  * 启动界面
  */
-public class SplashActivity extends AppCompatActivity{
+public class SplashActivity extends Activity {
+
+    int cc = 0, count = 0;
+    private int LENG = 3000;
+    private Runnable rr, ru;
+    Handler handler;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//去掉Activity上面的状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+        SharedPreferences preferences = getSharedPreferences("count",
+                MODE_WORLD_READABLE);
+        count = preferences.getInt("count", 0);
+        final int cc = count;
+        rr = new Runnable() {
+            @Override
+            public void run() {//进入主界面
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        };
+
+        handler = new Handler();
+        ru = new Runnable() {
+            @Override
+            public void run() {//进入引导界面
+                if (cc == 0) {
+                    Intent intent = new Intent();
+                    intent.setClass(getApplicationContext(), GuideActivity.class);
+                    startActivity(intent);
+                    SplashActivity.this.finish();
+
+                }
+                // 判断程序与第几次运行，如果是第一次运行则跳转到引导页面
+            }
+        };
+        SharedPreferences.Editor editor = preferences.edit();
+        // 存入数据
+        editor.putInt("count", ++count);
+        // 提交修改
+        editor.commit();
+        if (cc == 0)
+            handler.postDelayed(ru, LENG);// 跳入引导界面
+        if (count != 1)
+            handler.postDelayed(rr, LENG);// 跳入主界面
+
     }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            if (cc == 0 && count == 1) {//进入引导界面
+                Intent intent = new Intent(SplashActivity.this, GuideActivity.class);
+                startActivity(intent);
+                finish();
+            }
+            if (count != 1) {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+            if (rr != null)
+                handler.removeCallbacks(rr);
+            if (ru != null)
+                handler.removeCallbacks(ru);
+        }
+
+        return super.onTouchEvent(event);
+    }
+
 }
