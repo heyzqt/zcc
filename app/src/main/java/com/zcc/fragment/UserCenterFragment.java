c9a81dd50115b77a4b4ec779b05db6a0c7c4dda2
heyzqt <729224643@qq.com>
2016-09-07 15:15:11 +0800

    mainactivity usercenterfragment update 3

--- app/src/main/java/com/zcc/fragment/UserCenterFragment.java
+++ app/src/main/java/com/zcc/fragment/UserCenterFragment.java
@@ -6,8 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.zcc.activity.R;
+import com.zcc.widget.RoundImageView;
 
 /**
  * Created by heyzqt on 2016/9/6.
@@ -15,6 +18,36 @@ import com.zcc.activity.R;
  * 用户中心Fragment
  */
 public class UserCenterFragment extends Fragment {
+
+    /**
+     * 用户头像
+     */
+    private RoundImageView mUserHeadView;
+
+    /**
+     * 用户名
+     */
+    private TextView mUsernameTv;
+
+    /**
+     * 我的地址
+     */
+    private LinearLayout mAddressLy;
+
+    /**
+     * 我的订单
+     */
+    private LinearLayout mOrderLy;
+
+    /**
+     * 我的收藏
+     */
+    private LinearLayout mCollectLy;
+
+    /**
+     * 个人信息
+     */
+    private LinearLayout mUserInfoLy;
 
     @Nullable
     @Override
