2fc31ed0d8b1feb88a191c25d02258213b2c3d7d
heyzqt <729224643@qq.com>
2016-09-07 17:24:33 +0800

    shoppingcar update 1

--- app/src/main/java/com/zcc/fragment/ShoppingCarFragment.java
+++ app/src/main/java/com/zcc/fragment/ShoppingCarFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.zcc.activity.R;
 
@@ -16,10 +17,20 @@ import com.zcc.activity.R;
  */
 public class ShoppingCarFragment extends Fragment{
 
+    /**
+     * 购物车列表
+     */
+    private ListView mShoppingCarLv;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View contentView = inflater.inflate(R.layout.fragment_shoppingcar, null);
+        initView(contentView);
         return contentView;
+    }
+
+    private void initView(View view){
+        mShoppingCarLv = (ListView) view.findViewById(R.id.lv_shoppingcar);
     }
 }
