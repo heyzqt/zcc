5b761ee171635988499d475b20798de7a9baf8d3
heyzqt <729224643@qq.com>
2016-09-09 09:14:21 +0800

    LoginActivity and RegisterActivity update 1

--- app/src/main/java/com/zcc/activity/RegisterActivity.java
+++ app/src/main/java/com/zcc/activity/RegisterActivity.java
@@ -1,15 +1,26 @@
 package com.zcc.activity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.lidroid.xutils.exception.DbException;
 import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
+import com.zcc.ZccApplication;
+import com.zcc.dbutils.DBHelper;
+import com.zcc.entity.User;
+import com.zcc.utils.Utils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by heyzqt on 2016/9/6.
@@ -17,6 +28,8 @@ import com.loopj.android.http.RequestParams;
  * 注册界面
  */
 public class RegisterActivity extends AppCompatActivity {
+
+    private ImageView mIvBack;
 
     private EditText mEdtName;
 
@@ -28,6 +41,8 @@ public class RegisterActivity extends AppCompatActivity {
 
     private Button mBtnRegister;
 
+    private Context mContext;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,26 +52,85 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void init() {
+        mContext = this;
+        mIvBack = (ImageView) findViewById(R.id.image_rg_back);
         mEdtName = (EditText) findViewById(R.id.edit_reg_name);
         mEdtPhone = (EditText) findViewById(R.id.edit_reg_phone);
         mEdtPassword = (EditText) findViewById(R.id.edit_reg_password);
         mEdtPasswordCheck = (EditText) findViewById(R.id.edit_reg_password_check);
         mBtnRegister = (Button) findViewById(R.id.button_register);
 
+        mIvBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
         mBtnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = mEdtName.getText().toString();
-                String password = mEdtPassword.getText().toString();
+                final String name = mEdtName.getText().toString();
+                final String phone = mEdtPhone.getText().toString();
+                final String password = mEdtPassword.getText().toString();
                 String passwordcheck = mEdtPasswordCheck.getText().toString();
-                if(!password.equals(passwordcheck)){
+                if (name.equals("") || phone.equals("") || password.equals("") || passwordcheck.equals("")) {
+                    Toast.makeText(RegisterActivity.this, "不能留空！", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (!Utils.isMobileNum(phone)) {
+                    Toast.makeText(RegisterActivity.this, "手机号码格式错误!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (!password.equals(passwordcheck)) {
                     Toast.makeText(RegisterActivity.this, "两次输入的密码不一致！", Toast.LENGTH_SHORT).show();
-                }else{
+                    return;
+                } else {
                     AsyncHttpClient asyncHttp = new AsyncHttpClient();
                     RequestParams rp = new RequestParams();
-                    //rp.put("username",);
-                    //asyncHttp.post(mIPAddress,rp);
-                    Toast.makeText(RegisterActivity.this, "注册成功", Toast.LENGTH_SHORT).show();
+                    rp.put("name", name);
+                    rp.put("phone", phone);
+                    rp.put("password", password);
+                    rp.put("sex", "未知");
+                    asyncHttp.post(ZccApplication.IP_ADDRESS + "Login/user_reg", rp, new AsyncHttpResponseHandler() {
+                        @Override
+                        public void onSuccess(int i, String s) {
+                            try {
+                                JSONObject jsonObject = new JSONObject(s);
+                                String result = jsonObject.getString("code");
+                                if (result.equals("success")) {
+                                    Toast.makeText(RegisterActivity.this, "注册成功，请登录", Toast.LENGTH_SHORT).show();
+                                    //保存user对象
+                                    User user = new User();
+                                    user.setName(name);
+                                    user.setImgUrl("aliuser_place_holder");
+                                    user.setPassword(password);
+                                    user.setPhone(phone);
+                                    user.setSex("未知");
+                                    DBHelper.getInstance(mContext).save(user);
+
+                                    //将userId保存
+                                    ZccApplication.mUserId = user.getId();
+                                    ZccApplication.editor.putInt(ZccApplication.USERID_KEY, user.getId());
+                                    ZccApplication.editor.commit();
+                                    finish();
+                                } else {
+                                    Toast.makeText(RegisterActivity.this, "注册失败", Toast.LENGTH_SHORT).show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            } catch (DbException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Throwable throwable, String s) {
+                            Toast.makeText(RegisterActivity.this, "网络连接失败 :(", Toast.LENGTH_SHORT).show();
+                        }
+                    });
                 }
             }
         });
--- app/src/main/java/com/zcc/utils/Utils.java
+++ app/src/main/java/com/zcc/utils/Utils.java
@@ -2,6 +2,9 @@ package com.zcc.utils;
 
 import android.content.Context;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by 张艳琴 on 2016/9/7.
  * <p/>
@@ -34,5 +37,18 @@ public class Utils {
         int resId = mContext.getResources().getIdentifier(imageName, "mipmap", mContext.getPackageName());
         //如果没有在"mipmap"下找到imageName,将会返回0
         return resId;
+    }
+
+    /**
+     * 判断电话号码是否正确
+     *
+     * @param mobile
+     * @return
+     */
+    public static boolean isMobileNum(String mobile) {
+        Pattern p = Pattern
+                .compile("^[1]([3][0-9]{1}|59|58|88|89|84)[0-9]{8}$");
+        Matcher m = p.matcher(mobile);
+        return m.matches();
     }
 }
